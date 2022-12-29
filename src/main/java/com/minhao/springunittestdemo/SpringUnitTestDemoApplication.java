421f64ce2561dfc23f48194054009f14eb32c80d
marvin <hao.min1995@foxmail.com>
2022-12-29 16:08:18 +0800

    first init

---
+++ src/main/java/com/minhao/springunittestdemo/SpringUnitTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.minhao.springunittestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUnitTestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUnitTestDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minhao/springunittestdemo/SpringUnitTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhao.springunittestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUnitTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
